31cb01e8ea7155fc6ec7564f0e654ac3e537c10c
Fabiano Andrade <affatecnologia@outlook.com.br>
2025-08-19 18:21:07 -0300

    feat: adicionado api_key no header

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const PORT = 3000;
 
 const dataApi = require('./breeds-dogs.json');
 
-app.get('/api-breeds-dogs', (req, res) => {
+app.get('/api/breeds-dogs', (req, res) => {
+    const apiKey = req.headers['x-api-key'];
+    if (apiKey !== process.env.API_KEY){
+        return res.status(403).json({ erro: 'Acesso negado'});
+    }
     res.json(dataApi);
 });
 
