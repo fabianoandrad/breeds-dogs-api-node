165179c1d9a96fb3fa937b02c2fab766aace25b2
Fabiano Andrade <affatecnologia@outlook.com.br>
2025-08-20 23:32:51 -0300

    alterado informação de erro do acesso

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dataApi = require('./breeds-dogs.json');
 app.get('/api/breeds-dogs', (req, res) => {
     const apiKey = req.headers['x-api-key'];
     if (apiKey !== process.env.API_KEY){
-        return res.status(403).json({ erro: 'Acesso negado'});
+        return res.status(403).json({ auth: 'Acesso negado', info: 'Você não tem permissão para acessar esta API', status: '403'});
     }
     res.json(dataApi);
 });
