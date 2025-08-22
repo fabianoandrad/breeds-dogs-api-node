a7c224bcb408f423504582bbc2d14240ea2c98c8
Fabiano Andrade <affatecnologia@outlook.com.br>
2025-08-21 21:48:25 -0300

    implementado dotenv

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+
+require('dotenv').config();
+const port = process.env.PORT;
+const apiKey = process.env.API_KEY
 
 const dataApi = require('./breeds-dogs.json');
 
 app.get('/api/breeds-dogs', (req, res) => {
-    const apiKey = req.headers['x-api-key'];
-    if (apiKey !== process.env.API_KEY){
+    const apiKeyHeader = req.headers['x-api-key'];
+    if (apiKeyHeader !== apiKey){
         return res.status(403).json({ auth: 'Acesso negado', info: 'Você não tem permissão para acessar esta API', status: '403'});
     }
     res.json(dataApi);
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
